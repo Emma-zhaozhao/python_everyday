0e82b4c295a8f6c001e059ed8a9d6202adf8ad87
Emma-zhaozhao <43108432+Emma-zhaozhao@users.noreply.github.com>
2018-11-24 20:49:18 +0800

    Update coupon.py

--- coupon.py
+++ coupon.py
@@ -1,13 +1,13 @@
 import random, string
 
-
+# 生成14位码
 def coupon_creator(digit):
     coupon = ''
     for word in range(digit):
         coupon += random.choice(string.ascii_uppercase + string.digits)
     return coupon
 
-
+# 生成200个优惠码
 def two_hundred_coupons():
     data = ''
     digit = 14  #十四位优惠券码
@@ -17,7 +17,7 @@ def two_hundred_coupons():
 
     return data
 
-
+# 存储到文件中
 coupondata = open('coupondata.txt', 'w')
 coupondata.write(two_hundred_coupons())
 coupondata.close()
